19c6d194e3050b9bdb7820ae23a7bf30927f2d9c
Brendan Powers <bpowers@dreeshomes.com>
2022-07-09 17:36:01 -0400

    fix: working on logic for retrieving logged in user's relevant state information after signing in with discord

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,53 @@
 require("dotenv").config();
 const express = require("express");
-const passport = require("passport");
+const Users = require("../prisma") 
+
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 const router = express.Router();
+
+router.get('/current-username', async (req, res) => {
+    try {
+      res.set('Cache-Control', 'no-store, no-cache');
+  
+      if (!req.isAuthenticated()) {
+        return res.status(401).send('User is not authenticated');
+      }
+      try {
+        const userId = req.session.passport.user;
+        let user = null;
+  
+        if (typeof userId !== 'undefined') { // implement new query w/ primsa format
+          const user = await prisma.users.findUnique({
+            where: {
+              id: userId,
+            },
+            select: {
+              username: true,
+              discordId: true,
+              avatar: true
+            }
+          })
+        }
+  
+        if (!user) {
+          throw new Error({ message: 'No user with this user id.' });
+        }
+  
+        return res.send(user);
+      } catch (e) {
+        return req.session.destroy(() => {
+          res.status(500).send({
+            message: e
+          });
+        });
+      }
+    } catch {
+      return res.status(401).send({
+        message: 'No user is signed in.'
+      });
+    }
+  });
 
 module.exports = router;
