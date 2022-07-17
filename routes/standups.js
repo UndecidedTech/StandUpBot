483ac4af6fe2c9e5a5446e4e62ea38d14f6cb5e1
Brendan Powers <bpowers@dreeshomes.com>
2022-07-17 14:33:26 -0400

    update

--- routes/standups.js
+++ routes/standups.js
@@ -39,7 +39,7 @@ router.post("/join", async (req, res) => {
     }
 
     let updatedStandup = await prisma.standUps.update({
-        id: newStandup.id || selectedStandup.id
+        id: selectedStandup.id
       }, 
       {
         $push: {
