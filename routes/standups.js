fac20ecfd58dc215dcbed1cdabed69e76c09e16a
Brendan Powers <bpowers@dreeshomes.com>
2022-07-17 12:34:11 -0400

    update

--- routes/standups.js
+++ routes/standups.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-import createStandup from "../daily";
+const createStandup = require("../daily");
 
 const router = express.Router();
 
