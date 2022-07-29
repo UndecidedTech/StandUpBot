58d0d77d6f8fd428e50f7af7ecede6d2baee9360
Daniel Yang <danyang0724@gmail.com>
2022-07-28 20:48:20 -0400

    fix: idk

--- routes/standups.js
+++ routes/standups.js
@@ -6,24 +6,26 @@ const createStandup = require("../daily");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    // TODO check auth when we can integrate auth with frontend (it's a disaster)
-    // if (!req.isAuthenticated()) {
-    //   return res.status(401).send('User is not authenticated');
-    // }
-    try {
-      console.log("Am I here?");
-      const dailyStandup = await getDailyStandup();
-      if (!dailyStandup) {
-        // send message to channel or just reuse the create Standup function I expose from daily.js
-        console.log("Daily standup hasn't been created yet, create it and send standup message to users");
-        const newStandup = await createStandup()
-        return res.send(newStandup);
-      }
-
-      return res.send(dailyStandup);
-    } catch (e) {
-      console.error(e);
+  // TODO check auth when we can integrate auth with frontend (it's a disaster)
+  // if (!req.isAuthenticated()) {
+  //   return res.status(401).send('User is not authenticated');
+  // }
+  try {
+    console.log("Am I here?");
+    const dailyStandup = await getDailyStandup();
+    if (!dailyStandup) {
+      // send message to channel or just reuse the create Standup function I expose from daily.js
+      console.log(
+        "Daily standup hasn't been created yet, create it and send standup message to users"
+      );
+      const newStandup = await createStandup();
+      return res.send(newStandup);
     }
+
+    return res.send(dailyStandup);
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 router.post("/join", async (req, res) => {
@@ -35,29 +37,29 @@ router.post("/join", async (req, res) => {
     let userId = req.session.passport.user;
     let currUser = await prisma.users.findFirst({
       where: {
-        id: userId
-      }
-    })
+        id: userId,
+      },
+    });
     let selectedStandup = await getDailyStandup();
     let updatedStandup = await prisma.standUps.update({
       where: {
-        id: selectedStandup.id
+        id: selectedStandup.id,
       },
       data: {
         standupMembers: {
           create: {
-            userId: userId
-          }
-        }
+            userId: userId,
+          },
+        },
       },
       include: {
         standupMembers: {
           select: {
             user: true,
             tasks: true,
-          }
-        }
-      }
+          },
+        },
+      },
     });
 
     console.log("here: ", updatedStandup);
@@ -65,7 +67,7 @@ router.post("/join", async (req, res) => {
   } catch (err) {
     console.error(err);
   }
-})
+});
 
 router.post("/task", async (req, res) => {
   try {
@@ -78,40 +80,38 @@ router.post("/task", async (req, res) => {
     // update current standup with req.body
     let updatedStandupMembers = await prisma.standUpMembers.update({
       where: {
-        $and: [
-          { standUpsId: standupId },
-          { userId: userId }
-        ]
+        standUpsId: standupId,
+        userId: userId,
       },
       data: {
         tasks: {
           create: {
-            label: req.body.label
-          }
-        }
-      }
-    })
+            label: req.body.label,
+          },
+        },
+      },
+    });
 
     console.log(updatedStandupMembers);
     return res.send(updatedStandupMembers);
   } catch (err) {
     console.error(err);
   }
-})
+});
 
 async function getDailyStandup() {
   const standup = await prisma.standUps.findFirst({
     where: {
-      date: new Date().toLocaleDateString()
+      date: new Date().toLocaleDateString(),
     },
     include: {
       standupMembers: {
         select: {
           user: true,
-          tasks: true
-        }
-      }
-    }
+          tasks: true,
+        },
+      },
+    },
   });
   return standup;
 }
