6d48b6a8c7c455ba93c40d6fa9534ef1009184a1
Brendan Powers <bpowers.tech@gmail.com>
2022-08-02 00:26:10 -0400

    fix: more env variable nonsense

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ router.get("/", passport.authenticate("discord"));
 router.get("/redirect", passport.authenticate("discord", {
     failureRedirect: "/forbidden"
 }), (req, res) => {
-    res.redirect("http://localhost:3000/tasks");
+    res.redirect(process.env.NODE_ENV === "production" ? "https://standupbotapp.herokuapp.com/tasks" : "http://localhost:3000/tasks");
 })
 
 module.exports = router;
